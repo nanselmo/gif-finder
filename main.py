6bddcc500f8fc638e50b2940eaecae79a7c3d63b
nanselmo <nanselmo@nanselmo-macbookpro.roam.corp.google.com>
2015-08-13 09:46:11 -0400

    random gif and dealing with all search terms

--- main.py
+++ main.py
@@ -18,7 +18,9 @@ import webapp2
 import jinja2
 import os
 import json
+import logging
 from google.appengine.api import urlfetch
+from random import randint
 
 jinja_environment = jinja2.Environment(loader=
     jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -29,16 +31,22 @@ class MainHandler(webapp2.RequestHandler):
     	self.response.out.write(main_template.render())
     def post(self):
         base_url = "http://api.giphy.com/v1/gifs/search?q="
-        api_key_url = "&api_key=dc6zaTOxFJmzC&limit=10"
+        api_key_url = "&api_key=dc6zaTOxFJmzC"
         search = self.request.get('search')
         search_term = search + '%20hunger%20games'
         if search_term:
            url = base_url + search_term + api_key_url
            parsed_data = json.loads(urlfetch.fetch(url).content)
-           gif_url = parsed_data['data'][0]['images']['original']['url']
+           if parsed_data['data']:
+            random_index = randint(0,len(parsed_data['data']))
+            gif_url = parsed_data['data'][random_index]['images']['original']['url']
+            msg = "okay"
+           else:
+             msg ="not found"
+             gif_url=""
            template = jinja_environment.get_template('templates/complete.html')
            #self.response.out.write(url)
-           self.response.out.write(template.render({"search_term" : search, "url" : gif_url}))
+           self.response.out.write(template.render({"search_term" : search, "url" : gif_url, "msg": msg}))
         else:
            self.response.write(('Please enter a search term'))
 
